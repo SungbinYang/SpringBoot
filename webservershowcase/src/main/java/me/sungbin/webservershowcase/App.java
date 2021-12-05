4ce91a6f6e2b5e257d47f67ad9cf7bf4d27cef63
SungbinYang <ysb05222@naver.com>
2021-12-05 17:08:05 +0900

    독립적으로 실행 가능한 JAR

--- webservershowcase/src/main/java/me/sungbin/webservershowcase/App.java
+++ webservershowcase/src/main/java/me/sungbin/webservershowcase/App.java
@@ -18,19 +18,4 @@ public class App {
         SpringApplication.run(App.class, args);
     }
 
-//    @Bean
-//    public ServletWebServerFactory serverFactory() {
-//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-//        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-//
-//        return tomcat;
-//    }
-//
-//    private Connector createStandardConnector() {
-//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//        connector.setPort(8080);
-//
-//        return connector;
-//    }
-
 }
